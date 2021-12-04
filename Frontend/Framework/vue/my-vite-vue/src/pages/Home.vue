a051dea6b6b467f8605f61a9e1d8f957b0b10b3b
李德晟 <2696612575@qq.com>
2021-12-04 22:20:12 +0800

    1. commit

---
+++ Frontend/Framework/vue/my-vite-vue/src/pages/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3>
+      {{ name }}
+    </h3>
+    <button @click="changeTitle">点我切换标题</button>
+  </div>
+</template>
+
+<script lang="ts">
+import {ref} from "vue";
+
+export default {
+  name: "Home",
+  setup() {
+    const name = ref("Home")
+
+    function changeTitle() {
+      name.value = "world"
+    }
+
+    return {
+      name,
+      changeTitle
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
