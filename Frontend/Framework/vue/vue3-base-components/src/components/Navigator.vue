361610014e16813a0f9047b2826e9a54265994f8
李德晟 <2696612575@qq.com>
2021-12-09 22:10:12 +0800

    1. vue基础组件库开发项目搭建

---
+++ Frontend/Framework/vue/vue3-base-components/src/components/Navigator.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="navigator">
+    <ul>
+      <li v-for="(item,index) in navList" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {reactive} from "vue";
+
+export default {
+  name: "Navigator",
+  setup() {
+    const navList = reactive(['表单', '表格', '面包屑', '日历', '按钮', '提示框'])
+    return {
+      navList
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.navigator {
+  max-width: 1196px;
+  margin: 0 auto;
+  background-color: bisque;
+  height: 50px;
+
+
+  ul {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+
+    li {
+      font-weight: bold;
+      list-style: none;
+      width: 100%;
+      cursor: pointer;
+    }
+  }
+}
+</style>
---
+++ Frontend/Framework/vue/vue3-base-components/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <Navigator></Navigator>
+
+</template>
+
+<script lang="ts">
+import Navigator from "../components/Navigator.vue"
+
+export default {
+  name: "Home",
+  components: {
+    Navigator
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
