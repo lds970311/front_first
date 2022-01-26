3676c30cd42dfa33a557648195ad40342646d5f1
李德晟 <2696612575@qq.com>
2022-01-26 11:36:12 +0800

    1. 创建,构建maven java工程 2. 创建, 构建java web工程

---
+++ javaweb/first_maven/src/main/java/com/evan/Demo.java
@@ -0,0 +1,15 @@
+// Author : evan lee
+// Time ： 2022-01-25  21:28
+// FILENAME : Demo.java
+// STATEMENT: 
+
+package com.evan;
+
+import org.junit.Test;
+
+public class Demo {
+    @Test
+    public void test() {
+        System.out.println("123");
+    }
+}
