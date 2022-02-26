fbbafc4e3aea0ed2f35c252f515194a1ac404364
李德晟 <2696612575@qq.com>
2022-02-26 17:12:05 +0800

    1. nacos配置 远程拉取

--- micrioservice/cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ micrioservice/cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,10 +1,18 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.PatternProperties;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
@@ -13,6 +21,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PatternProperties patternProperties;
 
     /**
      * 路径： /user/110
@@ -23,5 +34,11 @@ public class UserController {
     @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Long id) {
         return userService.queryById(id);
+    }
+
+
+    @GetMapping("now")
+    public String getNowString() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
     }
 }
