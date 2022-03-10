1196c822b9505a23c5df029c27a3f944096c1fb0
李德晟 <2696612575@qq.com>
2022-03-10 13:26:32 +0800

    设计模式 1. 状态模式 2. 观察者模式

---
+++ java_advance/design_pattern/src/test/java/com/evanlee/behavior/WatcherTest.java
@@ -0,0 +1,25 @@
+// Author : evan lee
+// Time ： 2022-03-10  0:51
+// FILENAME : WatcherTest.java
+// STATEMENT: 
+
+package com.evanlee.behavior;
+
+import com.evanlee.watcher.Subject;
+import com.evanlee.watcher.SubscriptionSubject;
+import com.evanlee.watcher.WeixinUser;
+import org.junit.jupiter.api.Test;
+
+
+//观察者模式测试
+public class WatcherTest {
+    @Test
+    void subscribe() {
+        Subject subject = new SubscriptionSubject();
+        subject.attach(new WeixinUser("zs"));
+        subject.attach(new WeixinUser("ls"));
+        subject.attach(new WeixinUser("lucy"));
+        subject.attach(new WeixinUser("tom"));
+        subject.notify("今天天气咋样?");
+    }
+}
