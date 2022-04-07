ec2572c675be280ceb4f1cf425d649b5ceb311ef
李德晟 <2696612575@qq.com>
2022-04-07 08:45:03 +0800

    selenium获取界面元素操作

---
+++ automatic_test/selenuimExec/DriverInitial/DriverTest.py
@@ -0,0 +1,26 @@
+# _*_ coding : UTF-8 _*_
+# Author : CSR丶WARRIOR
+# Time ： 2022-04-06  16:32
+# TOOLS : Pycharm
+# FILENAME : DriverTest.py
+# STATEMENT:
+# 导入selenium
+import time
+
+from selenium import webdriver
+
+# 实例化浏览器驱动对象（创建浏览器驱动对象）
+driver = webdriver.Chrome()  # 创建的是谷歌浏览器驱动对象   chrome后面有括号，而且第一个字母要大写
+# driver = webdriver.Firefox() # 创建火狐浏览器驱动对象
+# 打开百度网站
+driver.get("https://www.163.com")
+btn = driver.find_elements_by_xpath(
+    "//ul[@class='cm_ul_round']/li[3]/a")
+
+time.sleep(3)
+print(btn)
+btn[0].click()
+# 等待3s（代表业务操作）
+time.sleep(5)  # 通过快捷导包的方式导入time模块，  光标要在time后面再按alt+enter
+# 退出浏览器驱动(释放系统资源)
+driver.quit()
