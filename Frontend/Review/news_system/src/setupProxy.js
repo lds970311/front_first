9549aafd06e1c7297f58c9a3033fa5ee77a621b9
李德晟 <2696612575@qq.com>
2022-05-08 02:11:21 +0800

    新闻发布管理系统 1. 初始化项目 2. 引入基本库,antd 3. 基础页面,路由配置完成

---
+++ Frontend/Review/news_system/src/setupProxy.js
@@ -0,0 +1,11 @@
+const {createProxyMiddleware} = require("http-proxy-middleware");
+
+module.exports = function (app) {
+    app.use(
+        "/ajax", // /api/list /api/detail /api/center /api/cart
+        createProxyMiddleware({
+            target: "https://i.maoyan.com",
+            changeOrigin: true,
+        })
+    );
+}
---
+++ Frontend/Review/news_system/src/views/NewsSandbox.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsSandbox = () => {
+    return (
+        <div>
+            NewsSandbox
+        </div>
+    );
+};
+
+export default NewsSandbox;
