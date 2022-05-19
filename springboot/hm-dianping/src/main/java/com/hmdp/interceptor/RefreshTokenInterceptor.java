9a38bd409a9fb0dbfeea86b294ddf640097116a1
李德晟 <2696612575@qq.com>
2022-05-19 19:08:14 +0800

    点评app 1. 完成商品详情查询功能, 解决缓存穿透问题 2. 完成商品类型查询, 添加缓存机制

---
+++ springboot/hm-dianping/src/main/java/com/hmdp/interceptor/RefreshTokenInterceptor.java
@@ -0,0 +1,57 @@
+// Author : evan lee
+// Time ： 2022-05-18  20:46
+// FILENAME : RefreshTokenInterceptor.java
+// STATEMENT: 
+
+package com.hmdp.interceptor;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class RefreshTokenInterceptor implements HandlerInterceptor {
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RefreshTokenInterceptor(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("authorization");
+        //HttpSession session = request.getSession();
+        if (StrUtil.isBlank(token)) {
+            return true;
+        }
+
+        //UserDTO user = (UserDTO) session.getAttribute("currentUser");
+        Map<Object, Object> entries = stringRedisTemplate.opsForHash().entries(RedisConstants.LOGIN_USER_KEY + token);
+        if (entries.isEmpty()) {
+            return true;
+        }
+        UserDTO user = BeanUtil.toBean(entries, UserDTO.class);
+        //基于token获取redis中的用户
+        if (user == null) {
+            response.setStatus(401);
+            return false;
+        }
+        //存在,保存用户到ThreadLocal
+        UserHolder.saveUser(user);
+        stringRedisTemplate.expire(RedisConstants.LOGIN_USER_KEY + token, 30, TimeUnit.MINUTES);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
