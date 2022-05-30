48f6c1e33db53bcadb1c04d3897a8ab91af2fa02
李德晟 <2696612575@qq.com>
2022-05-30 08:28:54 +0800

    1. 编写hadoop脚本(文件同步,jps,启动/关闭集群)

---
+++ bigData/hadoop/src/main/resources/scripts/jpsall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for host in hadoop149 hadoop131 hadoop132
+do
+ echo =============== $host ===============
+ ssh $host jps
+done
---
+++ bigData/hadoop/src/main/resources/scripts/refresh_node.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for host in hadoop149 hadoop131 hadoop132; do
+  echo =============== $host ===============
+  ssh $host
+  echo "removing $host data & logs"
+  cd "/usr/local/docker_images/hadoop/hadoop-3.3.3/" || exit
+  rm -rf data/ logs
+done
