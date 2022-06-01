869e9cd4add1ec38a09828b48af142dd29122fce
李德晟 <2696612575@qq.com>
2022-06-01 09:39:26 +0800

    1. MapReduce的wordcount案例

---
+++ bigData/hadoop/src/main/java/com/evan/mapreduce/wordcount/WordCountMapper.java
@@ -0,0 +1,38 @@
+// Author : evan lee
+// Time ： 2022-05-31  21:57
+// FILENAME : WordCountMapper.java
+// STATEMENT: 
+
+package com.evan.mapreduce.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * KEYIN, map阶段输入的key的类型：LongWritable
+ * VALUEIN,map阶段输入value类型：Text
+ * KEYOUT,map阶段输出的Key类型：Text
+ * VALUEOUT,map阶段输出的value类型：IntWritable
+ */
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private Text outKey = new Text();
+    private IntWritable outValue = new IntWritable();
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        //获取一行
+        String stringValue = value.toString();
+        //切割
+        String[] words = stringValue.split(" ");
+        //写出
+        for (String word : words) {
+            outKey.set(word);
+            outValue.set(1);
+            context.write(outKey, outValue);
+        }
+    }
+}
