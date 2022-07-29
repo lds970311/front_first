d87c62abe2f6abd4cb512fcead100a6e7fb4ab47
李德晟 <2696612575@qq.com>
2022-07-29 17:45:02 +0800

    1. 多线程交替打印练习

---
+++ java_advance/concurrent/src/main/java/cn/evan/exercise/AwaitSignal.java
@@ -0,0 +1,63 @@
+// Author : evan lee
+// Time ： 2022-07-27  20:43
+// FILENAME : AwaitSignal.java
+// STATEMENT: 
+
+package cn.evan.exercise;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Slf4j(topic = "c.AwaitSignal")
+public class AwaitSignal extends ReentrantLock {
+    // 循环次数
+    private int loopNumber;
+
+    public AwaitSignal(int loopNumber) {
+        this.loopNumber = loopNumber;
+    }
+
+    public static void main(String[] args) {
+        AwaitSignal as = new AwaitSignal(5);
+        Condition aWaitSet = as.newCondition();
+        Condition bWaitSet = as.newCondition();
+        Condition cWaitSet = as.newCondition();
+        new Thread(() -> {
+            as.print("a", aWaitSet, bWaitSet);
+        }).start();
+        new Thread(() -> {
+            as.print("b", bWaitSet, cWaitSet);
+        }).start();
+        new Thread(() -> {
+            as.print("c", cWaitSet, aWaitSet);
+        }).start();
+        as.start(aWaitSet);
+    }
+
+    public void start(Condition first) {
+        this.lock();
+        try {
+            log.debug("start");
+            first.signal();
+        } finally {
+            this.unlock();
+        }
+    }
+
+    public void print(String str, Condition current, Condition next) {
+        for (int i = 0; i < loopNumber; i++) {
+            this.lock();
+            try {
+                current.await();
+                log.debug(str);
+                next.signal();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                this.unlock();
+            }
+        }
+    }
+}
---
+++ java_advance/concurrent/src/main/java/cn/evan/exercise/SyncWaitNotify.java
@@ -0,0 +1,53 @@
+// Author : evan lee
+// Time ： 2022-07-27  20:36
+// FILENAME : SyncWaitNotify.java
+// STATEMENT: 
+
+package cn.evan.exercise;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "c.SyncWaitNotify")
+public class SyncWaitNotify {
+    /**
+     * 线程 1 输出 a 5 次，线程 2 输出 b 5 次，线程 3 输出 c 5 次。现在要求输出 abcabcabcabcabc 怎么实现
+     */
+
+    private int flag;//等待标记
+    private int loopNumber;
+
+    public SyncWaitNotify(int flag, int loopNumber) {
+        this.flag = flag;
+        this.loopNumber = loopNumber;
+    }
+
+    public static void main(String[] args) {
+        SyncWaitNotify syncWaitNotify = new SyncWaitNotify(1, 5);
+        new Thread(() -> {
+            syncWaitNotify.print(1, 2, "a");
+        }).start();
+        new Thread(() -> {
+            syncWaitNotify.print(2, 3, "b");
+        }).start();
+        new Thread(() -> {
+            syncWaitNotify.print(3, 1, "c");
+        }).start();
+    }
+
+    public void print(int waitFlag, int nextFlag, String str) {
+        for (int i = 0; i < loopNumber; i++) {
+            synchronized (this) {
+                while (this.flag != waitFlag) {
+                    try {
+                        this.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.print(str);
+                this.flag = nextFlag;
+                this.notifyAll();
+            }
+        }
+    }
+}
